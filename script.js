d722ebf01a69cf78c4fa3b721876b51a3765ab83
abel-udoh <abeludoh8@gmail.com>
2023-08-25 05:33:28 +0100

    kacmart

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-function resetAutoSlide() {
-    clearInterval(autoSlideInterval);
-    autoSlideInterval = setInterval(() => {
-      slideIndex = (slideIndex + 1) % sliderContent.children.length;
-      updateSlider();
-    }, 3000); // Change slide every 3 seconds (adjust as needed)
-  }
+let slideIndex = 1;
+showSlides(slideIndex);
 
-  // Initial auto slide setup
-  resetAutoSlide();
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
