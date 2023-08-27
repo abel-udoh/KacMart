249124549a114fcebee5e36473c1ab7f57b08a9b
abel-udoh <abeludoh8@gmail.com>
2023-08-27 07:19:31 +0100

    kacmart

--- script.js
+++ script.js
@@ -17,3 +17,30 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 4000); // Change image every 2 seconds
 }
+
+
+
+// Get the products section animate as user scroll down the scetion
+const productsSection = document.querySelector('#products1 .pro');
+
+// Function to check if an element is in the viewport
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Function to add animation class when the section is in viewport
+function handleScroll() {
+  if (isInViewport(productsSection)) {
+    productsSection.classList.add('products1.pro'); // Add the class that triggers the animation
+    window.removeEventListener('scroll', handleScroll); // Remove the scroll event listener once the animation is triggered
+  }
+}
+
+// Add a scroll event listener
+window.addEventListener('scroll', handleScroll);
