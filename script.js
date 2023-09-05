8039534dcf1189b76f1caf383665b788a629d39c
MikeTejas7 <ndubuisichime6@gmail.com>
2023-09-05 11:26:48 +0100

    About

--- script.js
+++ script.js
@@ -145,22 +145,6 @@ function updateCartTotal() {
   document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
 }
 
-// Add smooth scrolling to anchor links
-document.querySelectorAll('nav a').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-      e.preventDefault();
-
-      const targetId = this.getAttribute('href').substring(1);
-      const targetSection = document.getElementById(targetId);
-
-      if (targetSection) {
-          window.scrollTo({
-              top: targetSection.offsetTop - 50,
-              behavior: 'smooth'
-          });
-      }
-  });
-});
 
 
 
