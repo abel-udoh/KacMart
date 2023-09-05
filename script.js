d5a621e7873da23bcd24b46e130a9328071d7e14
MikeTejas7 <ndubuisichime6@gmail.com>
2023-09-05 11:10:12 +0100

    About us

--- script.js
+++ script.js
@@ -144,3 +144,26 @@ function updateCartTotal() {
   total = Math.round(total * 100) / 100
   document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
 }
+
+// Add smooth scrolling to anchor links
+document.querySelectorAll('nav a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute('href').substring(1);
+      const targetSection = document.getElementById(targetId);
+
+      if (targetSection) {
+          window.scrollTo({
+              top: targetSection.offsetTop - 50,
+              behavior: 'smooth'
+          });
+      }
+  });
+});
+
+
+
+
+
+
