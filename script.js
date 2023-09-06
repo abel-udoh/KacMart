93d797071fc7e8d0c6453ca856ef755ecc4f5466
abel-udoh <abeludoh8@gmail.com>
2023-09-06 05:46:18 +0100

    kamart

--- script.js
+++ script.js
@@ -123,6 +123,25 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 4000); // Change image every 2 seconds
 }
+
+
+/*for mobile navbar responsiveness*/
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+      nav.classList.add('active');
+    })
+  }
+
+if (close) {
+    close.addEventListener('click', () => {
+      nav.classList.remove('active');
+    })
+  }
+
 /*
 // Get the products section to animate as user scroll down the scetion
 window.addEventListener('scroll', reveal);
