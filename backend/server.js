cf54a757bd8d360be32dd7895621f9b8d2bfed5e
abel-udoh <abeludoh8@gmail.com>
2023-12-12 18:11:50 +0100

    setting up server

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+// server.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
+const { Sequelize } = require('sequelize');
+const sequelizeConfig = require('./sequelize.config');
+const UserModel = require('./models/user');
+
+const sequelize = new Sequelize(sequelizeConfig.development);
+const User = UserModel(sequelize);
+
+sequelize.sync({ force: false }).then(() => {
+  console.log('Database synced');
+});
